78cd5196f2596a53c9d0cbda46f04cf1acd21c3c
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-06-28 10:53:14 +0700

    4.9

--- main.js
+++ main.js
@@ -7,14 +7,18 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const des1 = ref("SUPPORTIVE SOCKS MADE WITH A BLEND OF RECYCLED AND RENEWABLE MATERIALS.")
         const des2 = ref("From daily training to the big occasions, adidas crew length socks keep you moving comfortably. They wrap around the arches of your feet and cushion high-pressure areas at the heels and toes. So you stay focused on performance, whether you're running, lifting or playing sports.")
-          const inStock = ref(true)
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = (true)
         return {
             product,
             productUrl,
             image,
             des1,
             des2,
-            inStock
+            inStock,
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
