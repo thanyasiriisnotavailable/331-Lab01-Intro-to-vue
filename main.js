d9cd42ef4beb7e7404e236a246558a96ddbaefa8
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-06-28 10:30:13 +0700

    from the start until 3.4

---
+++ main.js
@@ -0,0 +1,16 @@
+const { createApp, ref } = Vue
+
+createApp({
+    setup() {
+        const product =  ref('Boots')
+        const image = ref('./assets/images/socks_green.jpg')
+        const des1 = ref("SUPPORTIVE SOCKS MADE WITH A BLEND OF RECYCLED AND RENEWABLE MATERIALS.")
+        const des2 = ref("From daily training to the big occasions, adidas crew length socks keep you moving comfortably. They wrap around the arches of your feet and cushion high-pressure areas at the heels and toes. So you stay focused on performance, whether you're running, lifting or playing sports.")
+        return {
+            product,
+            image,
+            des1,
+            des2
+        }
+    }
+}).mount('#app')
